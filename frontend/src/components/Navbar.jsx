31130c9d32647ba74a5c5c035a2aae8e37026044
abdulazeem8879 <abdulazeemkhan8879@gmail.com>
2025-10-25 22:35:03 +0530

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/assets";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const [visible, setvisible] = useState(false);
+
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <img src={assets.logo} className="w-32" alt="" />
@@ -42,6 +44,17 @@ const Navbar = () => {
             </div>
           </div>
         </div>
+        <Link to="/cart" className="relative">
+          <img src={assets.cart_icon} className="w-5 min-w-5" alt="" />
+          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
+            10
+          </p>
+        </Link>
+        <img
+          src={assets.menu_icon}
+          className="w-5 cursor-pointer sm:hidden"
+          alt=""
+        />
       </div>
     </div>
   );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
     // <div className='text-red-500 text-5xl text-' >Welcome Home</div>
     <>
-    
+  
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
